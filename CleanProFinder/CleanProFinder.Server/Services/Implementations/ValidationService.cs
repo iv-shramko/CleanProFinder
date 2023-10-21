d3f6be7d848b73c92afed3918059f16993bf1f47
Illia Teliuk <illiateliuk@gmail.com>
2023-10-21 18:03:35 +0300

    CleanProFinder.Server: ValidationService implementation

---
+++ CleanProFinder/CleanProFinder.Server/Services/Implementations/ValidationService.cs
@@ -0,0 +1,29 @@
+﻿using CleanProFinder.Server.Services.Interfaces;
+using CleanProFinder.Shared.Errors.Base;
+using CleanProFinder.Shared.ServiceResponseHandling;
+using FluentValidation;
+using System.Reflection;
+
+
+namespace CleanProFinder.Server.Services.Implementations
+{
+    public class ValidationService : IValidationService
+    {
+        public async Task<ServiceResponse> ValidateAsync<T>(T item)
+        {
+            var validatorType = Assembly.GetExecutingAssembly().GetTypes()
+                .FirstOrDefault(t => t.BaseType != null && t.BaseType.IsGenericType &&
+                                     t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>) &&
+                                     t.BaseType.GetGenericArguments().Any(genericArg => genericArg == typeof(T)));
+
+
+            var validator = (IValidator)Activator.CreateInstance(validatorType);
+            var validationContext = new ValidationContext<T>(item);
+            var validationResult = await validator.ValidateAsync(validationContext);
+
+            var errors = validationResult.Errors.Select(error => new ValidationError() { FieldCode = error.PropertyName, ErrorMessage = error.ErrorMessage });
+
+            return ServiceResponseBuilder.Failure(new List<ValidationError>());
+        }
+    }
+}
