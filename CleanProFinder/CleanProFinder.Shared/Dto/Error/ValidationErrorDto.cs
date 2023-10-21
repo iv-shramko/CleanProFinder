b1140309abd94a90e6cbe81a5eaa080711626b9d
Mykhailo Sereda <mykhailo.sereda256@gmail.com>
2023-10-21 19:14:40 +0300

    CleanProFinder.Server: setup service response handling for api controller

---
+++ CleanProFinder/CleanProFinder.Shared/Dto/Error/ValidationErrorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanProFinder.Shared.Dto.Error
+{
+    public class ValidationErrorDto
+    {
+        public string FieldCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
