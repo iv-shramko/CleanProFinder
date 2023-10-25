9707fec3aa007ced8a382a06fca209e4b46c8c06
Artem Volovyi <artem.volovyi@nure.ua>
2023-10-25 17:14:06 +0300

    Add network config

--- CleanProFinder/CleanProFinder.Mobile/MauiProgram.cs
+++ CleanProFinder/CleanProFinder.Mobile/MauiProgram.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+﻿using System.Reflection;
 using CleanProFinder.Mobile.ViewModels;
 using CleanProFinder.Mobile.Views;
+using Microsoft.Extensions.Configuration;
 
 namespace CleanProFinder.Mobile
 {
@@ -17,9 +19,23 @@ namespace CleanProFinder.Mobile
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            var stream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("CleanProFinder.Mobile.Properties.appsettings.json");
+            
+            if (stream != null)
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonStream(stream)
+                    .Build();
+
+                builder.Configuration.AddConfiguration(config);
+            }
+
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+            
             builder.Services.AddTransient<RolePage>();
             builder.Services.AddTransient<RoleViewModel>();
 
