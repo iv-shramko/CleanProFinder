2e8561bd7b7bf2be0733acf6eea25edfec3ece5e
Illia Teliuk <illiateliuk@gmail.com>
2023-10-28 21:58:58 +0300

    CleanProFinder.Shared: validators settings, provider profile dto

--- CleanProFinder/CleanProFinder.Shared/Validators/Profile/EditProviderProfileCommandValidator.cs
+++ CleanProFinder/CleanProFinder.Shared/Validators/Profile/EditProviderProfileCommandValidator.cs
@@ -4,19 +4,22 @@ using System.Text.RegularExpressions;
 
 namespace CleanProFinder.Shared.Validators.Profile
 {
-    public class EditUserProfileCommandValidator : AbstractValidator<UserProfileDto>
+    public class EditProviderProfileCommandValidator : AbstractValidator<ProviderProfileDto>
     {
-        public EditUserProfileCommandValidator()
+        public EditProviderProfileCommandValidator()
         {
-            RuleFor(p => p.FirstName)
-                .NotNull();
-
-            RuleFor(p => p.LastName)
+            RuleFor(p => p.Name)
                 .NotNull();
 
             RuleFor(p => p.PhoneNumber)
-                .NotNull()
                 .Matches(new Regex(@"\+\d{12}$"));
+
+            RuleFor(p => p.Site)
+                .NotNull();
+
+            RuleFor(p => p.LogoUrl)
+                .NotNull();
+
         }
     }
 }
