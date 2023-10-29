83a076d01070e0b7b671fe6fd0ea8d732efec4c1
Artem Volovyi <artem.volovyi@nure.ua>
2023-10-29 19:22:34 +0200

    Add timeout error handling

--- CleanProFinder/CleanProFinder.Mobile/Services/HttpService.cs
+++ CleanProFinder/CleanProFinder.Mobile/Services/HttpService.cs
@@ -10,12 +10,17 @@ namespace CleanProFinder.Mobile.Services;
 
 public class HttpService : IHttpService
 {
+    private const int TimeoutSeconds = 5;
+
     private readonly HttpClient _httpClient;
     private readonly string _baseUrl;
 
     public HttpService(IConfiguration configuration)
     {
-        _httpClient = new HttpClient();
+        _httpClient = new HttpClient()
+        {
+            Timeout = TimeSpan.FromSeconds(TimeoutSeconds)
+        };
         _baseUrl = configuration["BaseUrl"];
     }
 
@@ -71,31 +76,33 @@ public class HttpService : IHttpService
 
     private async Task<ServiceResponse<T>> SendRequestAsync<T>(HttpRequestMessage request)
     {
-        var httpResponseMessage = await _httpClient.SendAsync(request);
-        var json = await httpResponseMessage.Content.ReadAsStringAsync();
-
-        if (httpResponseMessage.IsSuccessStatusCode)
+        try
         {
-            var result = JsonConvert.DeserializeObject<T>(json);
-            return ServiceResponseBuilder.Success(result);
-        }
+            var httpResponseMessage = await _httpClient.SendAsync(request);
+            var json = await httpResponseMessage.Content.ReadAsStringAsync();
 
-        var error = JsonConvert.DeserializeObject<Error>(json);
-        return ServiceResponseBuilder.Failure<T>(error);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var result = JsonConvert.DeserializeObject<T>(json);
+                return ServiceResponseBuilder.Success(result);
+            }
+
+            var error = JsonConvert.DeserializeObject<Error>(json);
+            return ServiceResponseBuilder.Failure<T>(error);
+        }
+        catch (TaskCanceledException)
+        {
+            var timeoutServiceError = new ServiceError
+            {
+                ErrorMessage = "Connection timed out."
+            };
+            return ServiceResponseBuilder.Failure<T>(timeoutServiceError);
+        }
     }
 
     private async Task<ServiceResponse> SendRequestAsync(HttpRequestMessage request)
     {
-        var httpResponseMessage = await _httpClient.SendAsync(request);
-        var json = await httpResponseMessage.Content.ReadAsStringAsync();
-
-        if (httpResponseMessage.IsSuccessStatusCode)
-        {
-            return ServiceResponseBuilder.Success();
-        }
-
-        var error = JsonConvert.DeserializeObject<Error>(json);
-        return ServiceResponseBuilder.Failure(error);
+        return await SendRequestAsync<object>(request);
     }
 
     private static string CreateQueryString(Dictionary<string, object> parameters)
