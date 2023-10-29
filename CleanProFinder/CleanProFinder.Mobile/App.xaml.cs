5c24f5d45773bf63e09617e5a1a8bc741fbf459a
Artem Volovyi <artem.volovyi@nure.ua>
2023-10-29 14:53:24 +0200

    Add page routing on authentication and role

--- CleanProFinder/CleanProFinder.Mobile/App.xaml.cs
+++ CleanProFinder/CleanProFinder.Mobile/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace CleanProFinder.Mobile
+﻿using CleanProFinder.Mobile.Services;
+
+namespace CleanProFinder.Mobile
 {
     public partial class App : Application
     {
-        public App()
+        public App(IAuthService authService)
         {
             InitializeComponent();
+            
+            authService.Initialize();
 
             MainPage = new AppShell();
+
+            if (authService.IsAuthenticated)
+            {
+                Shell.Current.GoToAsync(authService.IsCustomer
+                    ? "//CustomerStartingPage"
+                    : "//ServiceProviderStartingPage");
+            }
+            else
+            {
+                Shell.Current.GoToAsync("//RolePage");
+            }
         }
     }
 }
