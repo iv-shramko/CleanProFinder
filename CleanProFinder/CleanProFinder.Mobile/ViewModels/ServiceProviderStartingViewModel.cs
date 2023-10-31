37dd8082548d89bac8b53292f5914bc374040898
Коротун Дмитро <dmytro.korotun@nure.ua>
2023-10-31 21:06:18 +0100

    Fix ServiceProviderStartingPage bindings

--- CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceProviderStartingViewModel.cs
+++ CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceProviderStartingViewModel.cs
@@ -4,8 +4,6 @@ using System.Collections.ObjectModel;
 
 namespace CleanProFinder.Mobile.ViewModels;
 
-public partial class ServiceProviderStartingViewModel : ObservableObject
-{
     public class ServiceProviderStartingMockData
     {
         public string BuildingAddress { get; set; }
@@ -14,22 +12,55 @@ public partial class ServiceProviderStartingViewModel : ObservableObject
         public float Square { get; set; }
         public string Description { get; set; }
     }
+
+public partial class ServiceProviderStartingViewModel : ObservableObject
+{
     public ServiceProviderStartingViewModel()
     {
         Premises = new ObservableCollection<ServiceProviderStartingMockData>()
         {
             new ServiceProviderStartingMockData {
-                BuildingAddress = "Building Address",
-                CleaningSpaceType = "Cleaning Space Type",
-                City = "City",
+                BuildingAddress = "Building Address 1 ",
+                CleaningSpaceType = "Cleaning Space Type 1",
+                City = "City 1",
                 Square = (float)123.45,
                 Description = "Description Description Description Description Description Description Description Description Description Description" },
             new ServiceProviderStartingMockData {
-                BuildingAddress = "Building Address",
-                CleaningSpaceType = "Cleaning Space Type",
-                City = "City",
+                BuildingAddress = "Building Address 2",
+                CleaningSpaceType = "Cleaning Space Type 2",
+                City = "City 2",
                 Square = (float)543.21,
+                Description = "Description Description Description Description Description Description Description Description Description" },
+            new ServiceProviderStartingMockData {
+                BuildingAddress = "Building Address 3",
+                CleaningSpaceType = "Cleaning Space Type 3",
+                City = "City 3",
+                Square = (float)343.43,
+                Description = "Description Description Description Description Description Description Description Description" },
+            new ServiceProviderStartingMockData {
+                BuildingAddress = "Building Address 4",
+                CleaningSpaceType = "Cleaning Space Type 4",
+                City = "City 4",
+                Square = (float)1234.62,
+                Description = "Description Description Description Description Description Description Description Description Description" },
+            new ServiceProviderStartingMockData {
+                BuildingAddress = "Building Address 5",
+                CleaningSpaceType = "Cleaning Space Type 5",
+                City = "City 5",
+                Square = (float)33.24,
+                Description = "Description Description Description Description Description Description Description Description" },
+            new ServiceProviderStartingMockData {
+                BuildingAddress = "Building Address 6",
+                CleaningSpaceType = "Cleaning Space Type 6",
+                City = "City 6",
+                Square = (float)862.53,
                 Description = "Description Description Description Description Description Description Description Description Description Description" },
+            new ServiceProviderStartingMockData {
+                BuildingAddress = "Building Address 7",
+                CleaningSpaceType = "Cleaning Space Type 7",
+                City = "City 7",
+                Square = (float)6314.45,
+                Description = "Description Description Description Description Description Description Description Description Description" },
         };
     }
 
