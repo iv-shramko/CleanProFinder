4d36a54ac06ac499cd7e4b7954a13ef8bafcb1a8
Illia Teliuk <illiateliuk@gmail.com>
2023-10-31 19:35:10 +0200

    CleanProFinder.Db: fix string fields

--- CleanProFinder/CleanProFinder.Db/Models/ServiceUser.cs
+++ CleanProFinder/CleanProFinder.Db/Models/ServiceUser.cs
@@ -2,9 +2,9 @@
 {
     public class ServiceUser : Entity
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
