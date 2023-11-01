adeb39cfcb8ad8784a425c60988a994e466507c9
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-01 17:18:25 +0200

    Replace mock with real data in ServiceUserStartingPage

--- CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceUserStartingViewModel.cs
+++ CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceUserStartingViewModel.cs
@@ -1,62 +1,48 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using CleanProFinder.Mobile.Services;
+using CleanProFinder.Shared.Dto.Profile;
 
 namespace CleanProFinder.Mobile.ViewModels;
 
-public class ServiceUserStartingMockData
-{
-    public string CleaningServiceName { get; set; }
-    public string CleaningSpacePhoto { get; set; }
-    public float Price { get; set; }
-    public string PriceType { get; set; }
-    public string Description { get; set; }
-}
-
 public partial class ServiceUserStartingViewModel : ObservableObject
 {
-    public ServiceUserStartingViewModel() 
+    private readonly IProviderService _providerService;
+
+    public ServiceUserStartingViewModel(IProviderService providerService) 
     {
-        ServiceProviders = new ObservableCollection<ServiceUserStartingMockData>()
-        {
-            new ServiceUserStartingMockData {
-                CleaningServiceName = "Cleaning Service Name 1",
-                CleaningSpacePhoto = "dotnet_bot.png",
-                Price = (float)123.45,
-                PriceType = "$",
-                Description = "Description Description Description Description Description Description Description Description Description" },
-            new ServiceUserStartingMockData {
-                CleaningServiceName = "Cleaning Service Name 2",
-                CleaningSpacePhoto = "dotnet_bot.png",
-                Price = (float)543.21,
-                PriceType = "$",
-                Description = "Description Description Description Description Description Description Description Description Description Description" },
-            new ServiceUserStartingMockData {
-                CleaningServiceName = "Cleaning Service Name 3",
-                CleaningSpacePhoto = "dotnet_bot.png",
-                Price = (float)341.34,
-                PriceType = "$",
-                Description = "Description Description Description Description Description Description Description Description" },
-            new ServiceUserStartingMockData {
-                CleaningServiceName = "Cleaning Service Name 4",
-                CleaningSpacePhoto = "dotnet_bot.png",
-                Price = (float)823.27,
-                PriceType = "$",
-                Description = "Description Description Description Description Description Description Description Description Description Description" },
-            new ServiceUserStartingMockData {
-                CleaningServiceName = "Cleaning Service Name 5",
-                CleaningSpacePhoto = "dotnet_bot.png",
-                Price = (float)533.43,
-                PriceType = "$",
-                Description = "Description Description Description Description Description Description Description Description Description" },
-        };
+        _providerService = providerService;
+        _serviceProviders = new ObservableCollection<ProviderPreviewDto>();
+        IsRefreshing = true;
     }
 
     [ObservableProperty]
-    ObservableCollection<ServiceUserStartingMockData> _serviceProviders;
+    ObservableCollection<ProviderPreviewDto> _serviceProviders;
+
+    [ObservableProperty]
+    private bool _isRefreshing;
 
     [ObservableProperty]
     private string _searchQuery;
+
+    [RelayCommand]
+    private async void LoadServiceProviders()
+    {
+        IsRefreshing = true;
+
+        var response = await _providerService.GetServiceProvidersAsync();
+        
+        if (response.IsSuccess)
+        {
+            foreach (var serviceProvider in response.Result)
+            {
+                ServiceProviders.Add(serviceProvider);
+            }
+        }
+        
+        IsRefreshing = false;
+    }
 
     [RelayCommand]
     private void Search()
