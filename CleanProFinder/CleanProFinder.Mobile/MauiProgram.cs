41f2372aafd442c6c0bcc1caabfd085278869cd3
Коротун Дмитро <dmytro.korotun@nure.ua>
2023-11-02 07:54:53 +0100

    Add EditProfilePage

--- CleanProFinder/CleanProFinder.Mobile/MauiProgram.cs
+++ CleanProFinder/CleanProFinder.Mobile/MauiProgram.cs
@@ -59,6 +59,9 @@ namespace CleanProFinder.Mobile
             builder.Services.AddTransient<InitialEditProfilePage>();
             builder.Services.AddTransient<InitialEditProfileViewModel>();
 
+            builder.Services.AddTransient<EditProfilePage>();
+            builder.Services.AddTransient<EditProfileViewModel>();
+
             builder.Services.AddSingleton<IHttpService, HttpService>();
             builder.Services.AddSingleton<IAuthService, AuthService>();
             builder.Services.AddSingleton<IDialogService, DialogService>();
