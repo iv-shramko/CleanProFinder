b80cac2498fffb82a2b68f83295c792f6d343e41
Illia Teliuk <illiateliuk@gmail.com>
2023-11-15 20:59:02 +0200

    CleanProFinder.Shared: updated provider-service related dtos

--- CleanProFinder/CleanProFinder.Shared/Dto/CleaningServices/EditProviderServiceDto.cs
+++ CleanProFinder/CleanProFinder.Shared/Dto/CleaningServices/EditProviderServiceDto.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace CleanProFinder.Shared.Dto.CleaningServices
 {
-    public class AddProviderServiceDto : EditableCleaningServiceDto
+    public class EditProviderServiceDto
     {   
-        public Guid CleaningServiceProviderId { get; set; }
         public Guid CleaningServiceId { get; set; }
         public decimal Price { get; set; }
     }
