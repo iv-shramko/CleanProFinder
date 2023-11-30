09accae3624e9c4f0c08480014b2971e5b9c41f7
Коротун Дмитро <dmytro.korotun@nure.ua>
2023-11-30 13:07:38 +0100

    Add RequestStorage

---
+++ CleanProFinder/CleanProFinder.Mobile/Services/Interfaces/IRequestStorage.cs
@@ -0,0 +1,13 @@
+﻿using CleanProFinder.Shared.Dto.CleaningServices;
+
+namespace CleanProFinder.Mobile.Services.Interfaces;
+
+public interface IRequestStorage
+{
+    string PremiseId { get; set; }
+    List<CleaningServiceDto> Services { get; set; }
+    string Description { get; set; }
+    string ServiceProviderId { get; set; }
+
+    public void Reset();
+}
