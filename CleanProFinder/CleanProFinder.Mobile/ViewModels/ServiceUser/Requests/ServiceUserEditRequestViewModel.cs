e98ffdeff1a0780a39836f9a41693cbf2d58c466
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-03 11:08:12 +0200

    Add service providers to edit request page

--- CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceUser/Requests/ServiceUserEditRequestViewModel.cs
+++ CleanProFinder/CleanProFinder.Mobile/ViewModels/ServiceUser/Requests/ServiceUserEditRequestViewModel.cs
@@ -13,8 +13,7 @@ public partial class ServiceUserEditRequestViewModel : ObservableObject, IQueryA
     private readonly IDialogService _dialogService;
     private readonly IRequestService _requestService;
 
-    public ServiceUserEditRequestViewModel(
-        IDialogService dialogService, IRequestService requestService)
+    public ServiceUserEditRequestViewModel(IDialogService dialogService, IRequestService requestService)
     {
         _dialogService = dialogService;
         _requestService = requestService;
