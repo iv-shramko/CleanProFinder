888c8bf1bb4fab1e14c1edfe8f385c1d582c83f5
Mykhailo Sereda <mykhailo.sereda256@gmail.com>
2023-12-05 13:53:06 +0200

    Server: notification fix

--- CleanProFinder/CleanProFinder.Server/Hubs/NotificationHub.cs
+++ CleanProFinder/CleanProFinder.Server/Hubs/NotificationHub.cs
@@ -1,11 +1,15 @@
-﻿using CleanProFinder.Shared.Dto.Notifications;
-using CleanProFinder.Shared.Helpers;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace CleanProFinder.Server.Hubs
 {
     public class NotificationHub : Hub
     {
-
+        public override async Task OnConnectedAsync()
+        {
+            var identity = Context.User!.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            await Groups.AddToGroupAsync(Context.ConnectionId, identity);
+            await base.OnConnectedAsync();
+        }
     }
 }
