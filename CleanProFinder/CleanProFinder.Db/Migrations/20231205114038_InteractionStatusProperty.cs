a13517386851700533f8c72af8f5a34d869dbf66
Illia Teliuk <illiateliuk@gmail.com>
2023-12-05 14:11:12 +0200

    CleanProFinder.Db: interactions status property migration for request interaction

---
+++ CleanProFinder/CleanProFinder.Db/Migrations/20231205114038_InteractionStatusProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanProFinder.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class InteractionStatusProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InteractionStatus",
+                table: "RequestInteractions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InteractionStatus",
+                table: "RequestInteractions");
+        }
+    }
+}
