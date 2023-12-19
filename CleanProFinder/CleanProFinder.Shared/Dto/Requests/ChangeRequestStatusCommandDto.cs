826978e8ddf595f7e966815a2c0517e8a927b723
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-19 13:01:37 +0200

    Fix change request status command

---
+++ CleanProFinder/CleanProFinder.Shared/Dto/Requests/ChangeRequestStatusCommandDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanProFinder.Shared.Dto.Requests
+{
+    public class ChangeRequestStatusCommandDto
+    {
+        public Guid RequestId { get; set; }
+    }
+}
